941c39b6d76ea7cac92c53398bbbb3949e25c481
Noah Ramey <nrameyj1@gmail.com>
2024-10-12 20:17:55 -0400

    working on flask

---
+++ spark/flaskapi.py
@@ -0,0 +1,19 @@
+from flask import Flask
+import sparkwork as sparky
+
+
+app = Flask(__name__)
+
+def worker(): 
+    lst = [i for i in range(0,100)]
+    return lst
+
+
+@app.route("/")
+def hello_world():
+    return worker()
+
+
+
+
+    
